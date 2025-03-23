51c22f17c996a2117c57509674aa3a62ccbee37c
Damola Adeagbo <damola90@googlemail.com>
2025-03-23 21:39:32 +0000

    Add setup.py for install

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="tsr",
+    packages=[
+        "tsr",
+        "tsr.models",
+        "tsr.models.tokenizers",
+        "tsr.models.transformer",
+    ],
+    install_requires=[
+
+    ],
+    author="TripoAI",
+)
